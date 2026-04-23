fcb194fbac7e129c82013d516a3b2dd3ace006f7
Pushkar Mishra <karsh02526@gmail.com>
2026-04-23 22:36:43 +0530

    Update reel system

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -1,18 +1,119 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class Reel : MonoBehaviour
+public class ReelSimpleLoop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Serialized Fields
+
+    [SerializeField] private RectTransform container;
+
+    [SerializeField] private float maxSpeed = 1800f;
+    [SerializeField] private float startY = 90f;
+    [SerializeField] private float resetY = 850f;
+    [SerializeField] private float gap = 190f;
+
+    [SerializeField] private float minStopTime = 2f;
+
+    #endregion
+
+    #region Private Fields
+
+    private float currentSpeed = 0f;
+
+    private bool isSpinning = false;
+    private bool isStopping = false;
+
+    #endregion
+
+    #region Unity Lifecycle
+
+    private void Start()
     {
-        
+        container.anchoredPosition = new Vector2(0f, startY);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isSpinning || isStopping) return;
+
+        container.anchoredPosition += Vector2.up * currentSpeed * Time.deltaTime;
+
+        if (container.anchoredPosition.y >= resetY)
+        {
+            container.anchoredPosition = new Vector2(0f, startY);
+        }
     }
+
+    #endregion
+
+    #region Public API
+
+    public void StartSpin()
+    {
+        isSpinning = true;
+        isStopping = false;
+
+        DOTween.To(() => currentSpeed, x => currentSpeed = x, maxSpeed, 0.5f)
+            .SetEase(Ease.OutQuad);
+    }
+
+    public void StopSpin()
+    {
+        if (!isSpinning || isStopping) return;
+
+        isStopping = true;
+
+        float loopHeight = resetY - startY;
+
+        int index = Random.Range(0, 5);
+        float baseTarget = startY + (index * gap);
+
+        float currentY = container.anchoredPosition.y;
+        float normalizedY = startY + Mathf.Repeat(currentY - startY, loopHeight);
+
+        float targetY = baseTarget;
+
+        if (targetY <= normalizedY)
+        {
+            targetY += loopHeight;
+        }
+
+        float requiredDistance = (currentSpeed * minStopTime) / 2f;
+
+        float distance = targetY - currentY;
+
+        while (distance < requiredDistance)
+        {
+            targetY += loopHeight;
+            distance = targetY - currentY;
+        }
+
+        float duration = (2f * distance) / currentSpeed;
+
+        float virtualY = currentY;
+
+        DOTween.To(() => virtualY, x =>
+        {
+            virtualY = x;
+
+            float wrappedY = startY + Mathf.Repeat(virtualY - startY, loopHeight);
+            container.anchoredPosition = new Vector2(0f, wrappedY);
+
+        }, targetY, duration)
+        .SetEase(Ease.OutCubic)
+        .OnComplete(() =>
+        {
+            float finalY = startY + Mathf.Repeat(targetY - startY, loopHeight);
+            container.anchoredPosition = new Vector2(0f, finalY);
+
+            currentSpeed = 0f;
+            isSpinning = false;
+            isStopping = false;
+        });
+
+        DOTween.To(() => currentSpeed, x => currentSpeed = x, 0f, duration)
+            .SetEase(Ease.OutCubic);
+    }
+
+    #endregion
 }
